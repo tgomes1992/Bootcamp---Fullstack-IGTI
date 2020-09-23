c91d972b6cd3624fb4608eee5948aab86e014aff
Thiago Gomes <tt7.thiago@gmail.com>
2020-09-22 22:48:13 -0300

    commit inicial , abertura do repositório

---
+++ mod1/trabalho_prático/js/script.js
@@ -0,0 +1,19 @@
+var red = document.querySelector("#Red");
+var redv = document.querySelector("#redv");
+var green = document.querySelector("#Green");
+var greenv = document.querySelector("#greenv");
+var blue = document.querySelector("#Blue");
+var bluev = document.querySelector("#bluev");
+var square = document.querySelector(".square");
+
+function changevalue() {
+  redv.value = red.value;
+  greenv.value = green.value;
+  bluev.value = blue.value;
+
+  square.style.backgroundColor = "rgb(" + red.value + "," + green.value + "," + blue.value + ")"
+}
+
+
+
+changevalue()
