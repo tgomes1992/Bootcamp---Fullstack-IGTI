8021fa9a246ef232e0347ad9bdcd17672b552b9b
Thiago Gomes <tt7.thiago@gmail.com>
2021-03-19 00:33:31 -0300

    Pequenas refatorações

--- bootcamp/mod1/Desafio/frontend/js/script.js
+++ bootcamp/mod1/Desafio/frontend/js/script.js
@@ -1,15 +1,17 @@
 // variáveis
-
+let filter = []
 
 let resultados = document.querySelector("#result");
 let estatisticas = document.querySelector("#dados");
 let buscar = document.querySelector("#search");
-
+let sub = document.querySelector("#sub");
 
 // inclusão do event listener no botao de busca
 
-buscar.addEventListener("keyup", () => {
+sub.addEventListener("click", (event) => {
+    event.preventDefault();
     let buscaruser = getuser(buscar.value);
+
 
 
 });
@@ -34,7 +36,7 @@ async function getuser(string) {
         return {
             firstname: user.name.first,
             lastname: user.name.last,
-            picture: user.picture.thumbnail,
+            picture: user.picture.medium,
             age: user.dob.age,
             gender: user.gender
         }
@@ -44,17 +46,14 @@ async function getuser(string) {
         return fullname.includes(string)
 
     })
-    setTimeout(() => {
-        retorno.forEach(user => {
-            criar_user(user);
-        })
 
+    retorno.forEach(user => {
+        criar_user(user);
+    })
 
-    }, 1000)
+    filter = [...retorno]
 
-    calculos(retorno)
-
-    console.log(retorno)
+    console.log(filter);
 
     return allusers
 };
@@ -63,42 +62,20 @@ async function getuser(string) {
 getuser()
 
 
-// função para tratar o retorno do fetch
-
-
-
-// fetch("http://localhost:3001/users").then(res => {
-//     res.json().then(data => {
-//         console.log(data)
-//     })
-// })
-
-
-
-function shownames(data) {
-    let names = {
-        'first': data.name.first,
-        'last': data.name.last,
-    }
-    console.log(names);
-
-}
-
-
-
-// getuser()
-
-
 
 //funções que vão percorrer o dom
 // função para criar usuários na página ( percorrendo o dom)
 
 function criar_user(user) {
     usuario = document.createElement("li");
+    usuario.classList.add("center", "card-panel", "blue", "lighten-5", "justify", "flex");
+    usuario.textContent = user.firstname + " " + user.lastname + "," + user.age + " anos  ";
     imagem = document.createElement("img");
     imagem.src = user.picture;
-    usuario.innerHTML = user.firstname + " " + user.lastname + "," + user.age + " anos";
-    usuario.appendChild(imagem);
+    imagem.classList.add("responsive-img", "circle");
+
+    usuario.append(imagem);
+
     resultados.append(usuario);
 
 }
@@ -109,46 +86,45 @@ function criar_user(user) {
 
 function criar_estatisticas() {
     masculino = document.createElement("li");
+    masculino.classList.add("male");
     feminino = document.createElement("li");
+    feminino.classList.add("female");
     somadasidades = document.createElement("li");
+    somadasidades.classList.add("sumages");
     mediadasidades = document.createElement("li");
-    masculino.innerHTML = "Sexo Masculino: ";
+    mediadasidades.classList.add("ages")
+    masculino.textContent = "Sexo Masculino: ";
     feminino.innerHTML = "Sexo feminino: ";
     somadasidades.innerHTML = "Soma das Idades: ";
     mediadasidades.innerHTML = "Media das Idades: ";
-    estatisticas.append(masculino, feminino, somadasidades, mediadasidades)
+    estatisticas.append(masculino, feminino, somadasidades, mediadasidades);
 
 }
 
-criar_estatisticas();
-
-function calculos(user) {
-    let macho = 0;
-    let femea = 0;
-    let idades = 0;
-    console.log(user.gender)
-    user.forEach(item => {
-        idades += item.age
-        if (item.gender == "male") {
-            macho = macho + 1;
-        } else
-        if (item.gender == "female") {
-            femea = femea + 1
-        }
 
 
+function update_estatistics() {
+
+}
+
+
+
+function calculos(filter) {
+    const sumages = filter.reduce((accumulator, current) => {
+        return accumulator + current.age
+    }, 0);
+    const male = filter.filter(person => {
+        return person.gender == "male"
     })
-    console.log("macho: " + macho)
-    console.log("femea: " + femea)
-    console.log("idades: " + idades)
-
+    const female = filter.filter(person => {
+        return person.gender == "female"
+    })
+    const totalfilter = filter.length;
+    const avg = (sumages / totalfilter).toFixed(2)
+    return {
+        "sumages": sumages,
+        "male": male.length,
+        "female": female.length,
+        "avgages": avg
+    }
 }
-
-
-
-// to do
-
-// -acertar requisição 
-//criação do array no dom
-
-// fazer os exercícios
